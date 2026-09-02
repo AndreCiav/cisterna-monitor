2ff22b25f6907c2f73112049a9d930b94c0dec8f
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-02 21:35:55 +0200

    Change GITHUB_TOKEN in config.js
    Updated GITHUB_TOKEN to a new value for security.

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const GITHUB_REPO = "cisterna-monitor";
 const GITHUB_BRANCH = "main";
 
 //token per progetto
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
