f2cb0eea8a5af5040fee14fad6c0be41ee5801fc
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-02 22:02:56 +0200

    Update GITHUB_TOKEN in config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const GITHUB_REPO = "cisterna-monitor";
 const GITHUB_BRANCH = "main";
 
 //token per progetto
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
