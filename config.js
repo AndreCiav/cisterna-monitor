86674e025f644511463259baa3c37c82ba3606a8
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-02 20:30:46 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// CONFIGURAZIONE SITO
+const GITHUB_USER = "AndreCiav";
+const GITHUB_REPO = "cisterna-monitor";
+const GITHUB_BRANCH = "main";
+
+//token per progetto
+const GITHUB_TOKEN = "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";
