b043e4ddc34e7db9c92268bc05610c5cd5627645
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-02 18:46:21 +0200

    Aggiornamenti nuovi file

---
+++ config.js
@@ -0,0 +1,8 @@
+// CONFIGURAZIONE SITO
+const GITHUB_USER = "INSERISCI_USERNAME";
+const GITHUB_REPO = "INSERISCI_REPOSITORY";
+const GITHUB_BRANCH = "main";
+
+// ATTENZIONE: se lasci il token qui, sarà visibile a chiunque apra il sito.
+// Usalo solo se il repository/token sono dedicati a questo progetto.
+const GITHUB_TOKEN = "INSERISCI_TOKEN_GITHUB";
