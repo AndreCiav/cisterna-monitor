b916a7b234ea9e8d3ab38e1b82652697cc06fd25
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-02 19:41:38 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// CONFIGURAZIONE SITO
-const GITHUB_USER = "INSERISCI_USERNAME";
-const GITHUB_REPO = "INSERISCI_REPOSITORY";
-const GITHUB_BRANCH = "main";
-
-// ATTENZIONE: se lasci il token qui, sarà visibile a chiunque apra il sito.
-// Usalo solo se il repository/token sono dedicati a questo progetto.
-const GITHUB_TOKEN = "INSERISCI_TOKEN_GITHUB";
