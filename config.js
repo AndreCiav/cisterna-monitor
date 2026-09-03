b38d4b47ecbea78b72b0bfb5b0fe72828b111836
AndreCiav <127783930+AndreCiav@users.noreply.github.com>
2026-09-03 12:36:59 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// CONFIGURAZIONE SITO
-const GITHUB_USER = "AndreCiav";
-const GITHUB_REPO = "cisterna-monitor";
-const GITHUB_BRANCH = "main";
-
-//token per progetto
-const GITHUB_TOKEN = "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";
